25f0379a60cf2e535e1b7731e207dbd0d5be8b2f
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-03-09 22:12:10 +0300

    add some js logic

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
-const menuButton = document.getElementById('menu-button');
+document.addEventListener('click', () => {
+    document.getElementById("drop-list").classList.remove("show"); 
+})
 
-menuButton.addEventListener('click', () => {
+
+const menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', (event) => {
   document.getElementById("drop-list").classList.toggle("show");
+  event.stopImmediatePropagation();
 });
 
 const formsButton = document.getElementById('form-button');
 formsButton.addEventListener('click', () => {
-  document.getElementById("form-filters").classList.toggle("show");
+  document.getElementById("form-filters").classList.toggle("hide");
   document.querySelector("section").classList.toggle("section-padding");
 })
-
-/*    const dropdown = document.getElementById('drop-list');
-    if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-    } else {
-        dropdown.classList.add('show');
-    }
-});
-
-
-/* function openMenu() {
-    document.getElementById("drop-list").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-    if (!event.target.matches('.drop-button')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-
-        for (let i= 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}; */
