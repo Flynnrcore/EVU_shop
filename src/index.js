63f28f0868b08d6e6196e040122301ac2ae43ee4
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-07-28 20:28:27 +0300

    change design

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-document.addEventListener('click', () => {
-    document.getElementById("drop-list").classList.remove("show"); 
-})
+import i18n from './i18next.js';
+import ru from './locales/ru.js';
 
+const candles = [
+  { name: 'blueberry', size: 'big', price: '1800' },
+  { name: 'cappuccino', size: 'big', price: '1800'},
+  { name: 'firework', size: 'big', price: '1800'},
+  { name: 'grass', size: 'big', price: '1800'},
+  { name: 'lavender', size: 'big', price: '1800'},
+  { name: 'nuts', size: 'big', price: '1800'},
+  { name: 'blueberry', size: 'small', price: '1200' },
+  { name: 'cappuccino', size: 'small', price: '1200'},
+  { name: 'firework', size: 'small', price: '1800'},
+  { name: 'grass', size: 'small', price: '1200'},
+  { name: 'lavender', size: 'small', price: '1200'},
+  { name: 'nuts', size: 'small', price: '1200'},
+];
 
-const menuButton = document.getElementById('menu-button');
-menuButton.addEventListener('click', (event) => {
-  document.getElementById("drop-list").classList.toggle("show");
-  event.stopImmediatePropagation();
+i18n.init({
+  lng: 'ru',
+  resources: {
+    ru,
+  },
 });
 
-const formsButton = document.getElementById('form-button');
-formsButton.addEventListener('click', () => {
-  document.getElementById("form-filters").classList.toggle("hide");
-  document.querySelector("section").classList.toggle("section-padding");
-})
+const itemsList = document.querySelector('.items');
+const renderItemCards = () => {
+  candles.map((candle) => {
+    const item = document.createElement('div');
+    item.classList.add('item-card');
+    const { name, size, price } = candle;
+    item.innerHTML = `<img class="item-image" src="./image/${size}-${name}.png" alt="candle ${name} pic">
+    <p class="item-name">Ароматическая свеча<br>"${i18n.t(name)}"</p>
+    <p class="item-price">${price} руб.</p>
+    <button class="item-button"><a class="item-button-link">${i18n.t('addToCart')}</a></button>`;
+    itemsList.append(item);
+  });
+};
+
+renderItemCards();
---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+    translation: {
+        blueberry: 'Черника со сливками',
+        cappuccino: 'Ароматный капучино',
+        firework: 'Экзотический фейерверк',
+        grass: 'Скошенная трава',
+        lavender: 'Лаванда и ваниль',
+        nuts: 'Nuts',
+        addToCart: 'добавить в корзину',
+    },
+  };
